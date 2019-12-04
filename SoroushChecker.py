090cf4d93685dba321b1d050179a990023a3a7f2
soroush <soazre@yahoo.ca>
2019-12-03 20:27:32 -0800

    Adding Soroush checker to the repository

---
+++ SoroushChecker.py
@@ -0,0 +1,23 @@
+import sys
+
+def Find(los):
+    endResult=False
+    number=0
+    for name in los:
+        number=number+1
+
+        if (name=="Soroush"):
+            endResult=True
+            print(number)
+            break
+
+
+    if(endResult==True):
+
+        print("I found Soroush :)")
+    else:
+        print("I didn't find Soroush")
+Find(sys.argv)
+
+
+
